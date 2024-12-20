df11932a3d48db11098f7ae95a6ee232c7c43239
unknown <benjafran2000@gmail.com>
2024-12-20 19:23:36 -0300

    Add tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-svg': "url('/bg.svg')",
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-};
+}
+
